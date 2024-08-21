8a0463ff8fa2f0d2d0dae3bddfad01eac7972ff0
Itohan <momoduitohan5@gmail.com>
2024-08-22 00:26:01 +0100

    FIFI Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,12 +7,24 @@ class FIFOCache(BaseCaching):
     """"""
 
     def __init__(self):
-        """"""
+        """Overloading"""
 
         super().__init__()
 
     def put(self, key, item):
-        """"""
+        """Updating the dictionary"""
 
-        if key or item is not None:
+        if key and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                key, value = list(self.cache_data.items())[0]
+                print(f"DISCARD: {key}")
+                del self.cache_data[next(iter(self.cache_data))]
 
+    def get(self, key):
+        """Returns value linked to key"""
+
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data.value()
