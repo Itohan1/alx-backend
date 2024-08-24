69a0f601c9e5eecf908ceeaf2703cd57dc4eee58
Itohan <momoduitohan5@gmail.com>
2024-08-24 15:57:58 +0100

    a class LFUCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""""""
+"""Discard the least frequency used item (LFU algorithm)"""
+from collections import defaultdict, OrderedDict
 BaseCaching = __import__("base_caching").BaseCaching
 
 
@@ -7,40 +8,49 @@ class LFUCache(BaseCaching):
     """"""
 
     def __init__(self):
-        """"""
+        """Initialize and overload the parent class"""
 
         super().__init__()
-        self.d_count = []
+        self.d_count = defaultdict(int)
+        self.usage = OrderedDict()
 
     def put(self, key, item):
-        """"""
+        """Add an item to dictionary"""
 
         if key is None or item is None:
             return
 
         if key in self.cache_data:
+            self.cache_data[key] = item
             self.d_count[key] += 1
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            min_use = min(self.d_count.values())
-
-            d_key = [k for k, v in self.cache_id.items() if v == min_use]
-            k_key = d_key[0]
-            del self.cache_data[k_key]
-            del self.d_count[k_key]
-            print(f"DISCARD: {k_key}")
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_use = min(self.d_count.values())
+                d_key = [k for k, v in self.d_count.items() if v == min_use]
+                if len(d_key) == 1:
+                    k_key = d_key[0]
+                else:
+                    for k in self.usage:
+                        if k in d_key:
+                            k_key = k
+                            break
+                del self.cache_data[k_key]
+                del self.d_count[k_key]
+                self.usage.pop(k_key, None)
+                print(f"DISCARD: {k_key}")
         self.cache_data[key] = item
         self.d_count[key] = 1
 
+        self.usage.pop(key, None)
+        self.usage[key] = None
+
     def get(self, key):
-        """"""
+        """Get item value by key"""
 
         if key is None or key not in self.cache_data:
             return None
 
-        for i in self.order:
-            for k, value in i.items():
-                if key == i.keys():
-                    self.order.remove(i)
-                    i[key] = value + 1
-
+        self.d_count[key] += 1
+        self.usage.pop(key, None)
+        self.usage.[key] = None
         return self.cache_data.get(key)
