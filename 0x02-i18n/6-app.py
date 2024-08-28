a1a36459447dde6d74c456b4d341934e6307ac6d
Itohan <momoduitohan5@gmail.com>
2024-08-28 21:53:34 +0100

    Use user locale

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Config for Flask app  to determine the best match"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns best match of supported language"""
+
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """Find the user"""
+
+    user_id = request.args.get("login_as")
+
+    if user_id and user_id.isdigit():
+        user_id = int(user_id)
+        return users.get(user_id)
+    return None
+
+
+@app.before_request
+def before_request():
+    """Get user before any request is made"""
+
+    g.user = get_user()
+
+
+@app.route("/")
+def first_method():
+    """To provide the correct the display for each message"""
+
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
