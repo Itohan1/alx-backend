9e436498081eb57d01f1b283e3770c871cb148b7
Itohan <momoduitohan5@gmail.com>
2024-08-28 10:50:03 +0100

    Config for Flask app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""Basic Babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    """Config for Flask app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+babel = Babel(app)
+
+
+@app.route("/")
+def first_method():
+    """html template"""
+
+    render.template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
